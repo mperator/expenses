d327bf13e13873997ff6f3f6b49faf4e87b50e98
Patrick Mutter <develop@mperia.de>
2020-11-11 21:12:02 +0100

    Add identity and db context.

---
+++ backend/Expenses.Api/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Expenses.Api.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Expenses.Api.Data
+{
+    public class AppDbContext : IdentityDbContext<User>
+    {
+        #region Constructors
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        #endregion
+    }
+}
