9e4fdb07e1f28394824af4157c7cf13b8d2ac3f4
mperator <develop@mperia.de>
2020-11-11 21:12:02 +0100

    Add identity and db context.

---
+++ backend/Expenses.Api/Entities/User.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace Expenses.Api.Entities
+{
+    public class User : IdentityUser
+    {
+        #region Properties
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime? DateOfBirth { get; set; }
+
+        #endregion
+    }
+}
