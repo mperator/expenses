3dde13c64cdfab2adf6d13d80f7995b0d6f8b9bc
Patrick Mutter <develop@mperia.de>
2020-11-12 21:14:42 +0100

    Add registration of user.

--- backend/Expenses.Api/Controllers/AuthController.cs
+++ backend/Expenses.Api/Controllers/AuthController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Expenses.Api.Entities;
+using Expenses.Api.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,9 +13,43 @@ namespace Expenses.Api.Controllers
     [Route("api/[controller]")]
     public class AuthController : ControllerBase
     {
-        public void Register()
-        {
+        private readonly UserManager<User> _userManager;
 
+        public AuthController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        /// <summary>
+        /// Registers a new user to Expenses.
+        /// </summary>
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterAsync([FromBody] UserRegistrationModel model)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if(await _userManager.FindByNameAsync(model.Username) != null) return BadRequest("Username is invalid or already taken.");
+            if (await _userManager.FindByEmailAsync(model.Email) != null) return BadRequest("Email is invalid or already taken.");
+
+            // add user to context send user an email he needs to verify
+            var result = await _userManager.CreateAsync(new User
+            {
+                UserName = model.Username,
+                Email = model.Email,
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                DateOfBirth = model.DateOfBirth
+            }, model.Password);
+
+            if(!result.Succeeded)
+            {
+                return BadRequest(result.Errors);
+            }
+
+            return NoContent();
         }
 
         public void Login()
