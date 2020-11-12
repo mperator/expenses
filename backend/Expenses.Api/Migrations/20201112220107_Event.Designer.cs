16f368e27e814a389da264d9a6bfa2427a3c7bf0
Xalip <thirsty.camel43@gmail.com>
2020-11-12 23:25:10 +0100

    added EventsController, EventModels and EventDtos; added GetEvents, CreateEvent

--- backend/Expenses.Api/Migrations/20201112220107_Event.Designer.cs
+++ backend/Expenses.Api/Migrations/20201112220107_Event.Designer.cs
@@ -4,14 +4,16 @@ using Expenses.Api.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Expenses.Api.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201112220107_Event")]
+    partial class Event
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
