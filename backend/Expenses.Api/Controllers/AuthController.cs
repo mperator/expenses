50e6ba745cad55f760cc860eb0a353c1554f8fb1
Patrick Mutter <develop@mperia.de>
2020-11-12 19:58:59 +0100

    Add auth controller.

---
+++ backend/Expenses.Api/Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Expenses.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        public void Register()
+        {
+
+        }
+
+        public void Login()
+        {
+
+        }
+
+        public void Logout()
+        {
+
+        }
+
+        public void RefreshToken()
+        {
+
+        }
+    }
+}
