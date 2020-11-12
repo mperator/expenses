e0a0001970b4bd36961380cad93afcaa0e705e8b
Patrick Mutter <develop@mperia.de>
2020-11-12 21:15:02 +0100

    Add token generation and validation.

---
+++ backend/Expenses.Api/Models/TokenModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Expenses.Api.Models
+{
+    public class TokenModel
+    {
+        public string TokenType { get; set; }
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+        public DateTime ExpiryDate { get; set; }
+    }
+}
---
+++ backend/Expenses.Api/Options/JwtTokenOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace Expenses.Api.Options
+{
+    public class JwtTokenOptions
+    {
+        public string SecretKey { get; set; }
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+    }
+}
