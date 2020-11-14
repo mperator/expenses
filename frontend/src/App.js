be01082170c2d7779b33390edd51dfbd71248fdf
Patrick Mutter <develop@mperia.de>
2020-11-14 23:30:55 +0100

    Add registration page.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 // import './App.css';
 
 import Login from './components/Login'
+import Register from './components/Register'
 
 function App() {
   return (
     <div className="App">
       <Login />
+      <Register />
     </div>
   );
 }
