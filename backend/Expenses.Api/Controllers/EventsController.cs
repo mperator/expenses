a5319c1b770e617677e847e7ff52025b6ce5f70e
Xalip <thirsty.camel43@gmail.com>
2020-11-16 22:12:47 +0100

    improved method documentation

--- backend/Expenses.Api/Controllers/EventsController.cs
+++ backend/Expenses.Api/Controllers/EventsController.cs
@@ -5,11 +5,14 @@ using AutoMapper;
 using Expenses.Api.Data;
 using Expenses.Api.Data.Dtos;
 using Expenses.Api.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace Expenses.Api.Controllers
 {
+    //TODO: implement clear error messages and return them to the user
+    [Authorize]
     [ApiController]
     [Route("api/[controller]")]
     public class EventsController : Controller
@@ -24,19 +27,21 @@ namespace Expenses.Api.Controllers
         /// <summary>
         /// Gets a list of events
         /// </summary>
-        /// <returns></returns>
+        /// <returns>A list of events</returns>
+        /// <response code="200">On success</response>
         [HttpGet]
         public async Task<ActionResult<List<EventReadModel>>> GetEventsAsync()
         {
-            //FIXME: check whether requesting user is authorized
-            // if not return 401 Unauthorized
             return Ok(_mapper.Map<List<EventReadModel>>(await _dbContext.EventData.ToListAsync()));
         }
         /// <summary>
         /// Get a single event by its ID
         /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
+        /// <param name="id">Unique ID to identify event</param>
+        /// <returns>A single event object</returns>
+        /// <response code="200">On success</response>
+        /// <response code="400">No ID given</response>
+        /// <response code="404">No resource found for the given ID</response>
         [HttpGet("{id}", Name = nameof(GetEventByIdAsync))]
         public async Task<ActionResult<EventReadModel>> GetEventByIdAsync(int? id)
         {
@@ -48,22 +53,21 @@ namespace Expenses.Api.Controllers
             return Ok(_mapper.Map<EventReadModel>(foundEvent));
         }
         /// <summary>
-        /// creates a new event
+        /// Creates a new event
         /// </summary>
-        /// <param name="eventModel"></param>
-        /// <returns></returns>
+        /// <param name="eventModel">Event which shall be created</param>
+        /// <returns>Created event object</returns>
+        /// <response code="400">Mapping failed or model isn't valid</response>
+        /// <response code="201">Returns created event object</response>
         [HttpPost]
         public async Task<ActionResult<EventReadModel>> CreateEventAsync([FromBody] EventWriteModel eventModel)
         {
-            //FIXME: check for authorization
-
             if (!ModelState.IsValid) return BadRequest(ModelState);
 
             var newEvent = _mapper.Map<Event>(eventModel);
 
-            //TODO: what to put inside BadRequest?
             if (newEvent == null) return BadRequest();
-            //Event newEvent;
+            
             Event savedEvent = new Event
             {
                 Title = newEvent.Title,
@@ -83,14 +87,16 @@ namespace Expenses.Api.Controllers
             {
                 new InvalidOperationException(e.Message);
             }
-            return Ok(savedEvent);
+            return CreatedAtRoute(nameof(GetEventByIdAsync), new { id = savedEvent.Id }, savedEvent);
         }
         /// <summary>
-        /// update an event by replacing it
+        /// Update an event by replacing it
         /// </summary>
-        /// <param name="id"></param>
-        /// <param name="model"></param>
-        /// <returns></returns>
+        /// <param name="id">ID of event which shall be updated</param>
+        /// <param name="model">Event data to update</param>
+        /// <response code="400">Model isn't valid or mapping failed</response>
+        /// <response code="404">No event to update found for the given ID </response>
+        /// <response code="204">On success</response>
         [HttpPut("{id}")]
         public async Task<ActionResult> UpdateEventAsync(int? id, [FromBody] EventUpdateModel model)
         {
@@ -104,7 +110,7 @@ namespace Expenses.Api.Controllers
 
             var dbEvent = await _dbContext.EventData.FirstOrDefaultAsync(ev => ev.Id == id);
 
-            if (dbEvent == null) return BadRequest();
+            if (dbEvent == null) return NotFound();
 
             dbEvent.Title = update.Title;
             dbEvent.StartDate = update.StartDate;
@@ -118,10 +124,12 @@ namespace Expenses.Api.Controllers
             return NoContent();
         }
         /// <summary>
-        /// delete an event using its ID
+        /// Delete an event using its ID
         /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
+        /// <param name="id">ID of event which shall be deleted</param>
+        /// <response code="400">No ID given</response>
+        /// <response code="404">No event to delete found for the given ID</response>
+        /// <response code="204">On success</response>
         [HttpDelete("{id}")]
         public async Task<ActionResult> DeleteEventByIdAsync(int? id)
         {
@@ -129,7 +137,7 @@ namespace Expenses.Api.Controllers
 
             var dbEvent = await _dbContext.EventData.FirstOrDefaultAsync(ev => ev.Id == id);
 
-            if (dbEvent == null) return BadRequest();
+            if (dbEvent == null) return NotFound();
 
             _dbContext.Remove(dbEvent);
             await _dbContext.SaveChangesAsync();
