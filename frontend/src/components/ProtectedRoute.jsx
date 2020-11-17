3e8273c3ec708d5b944756f4d1cf5d37f222067f
mperator <develop@mperia.de>
2020-11-17 22:36:35 +0100

    Add simple implementation of refresh token when user refreshes page.

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -4,18 +4,33 @@ import { Route, Redirect } from 'react-router-dom'
 import useAuth from '../hooks/useAuth'
 
 const ProtectedRoute = ({ component: Component, ...rest }) => {
-    const { isSignedIn } = useAuth();
+    const { isSignedIn, silentSignedInFailed, signInSilent } = useAuth();
+
+    function dodo(props) {
+        if (isSignedIn) {
+            return <Component {...props} {...rest} />
+        } 
+        else if(silentSignedInFailed) {
+            return <Redirect to={{ pathname: '/unauthorized', state: { from: props.location } }} />
+        } 
+        else {
+            signInSilent()
+            return <Component {...props} {...rest} />
+        }
+    }
+
+    // if(!isSignedIn) {
+
+    //     console.log("silent 1")
+
+    //     // silent sign in
+    //     //signInSilent();
+
+    //     console.log("silent 2")
+    // }
 
     return (
-        <Route {...rest} render={
-            props => {
-                if (isSignedIn) {
-                    return <Component {...props} {...rest} />
-                } else {
-                    return <Redirect to={{ pathname: '/unauthorized', state: { from: props.location } }} />
-                }
-            }
-        } />
+        <Route {...rest} render={props => dodo(props)} />
     )
 }
 
