865af080801273488f19d3efaa9fa9e9d5877825
Philipp Halbritter <p.halbritter@logistikbroker.com>
2020-11-20 23:12:28 +0100

    intermediate expensescontroller

--- backend/Expenses.Api/Controllers/ExpensesController.cs
+++ backend/Expenses.Api/Controllers/ExpensesController.cs
@@ -1,6 +1,9 @@
 ﻿using AutoMapper;
 using Expenses.Api.Data;
+using Expenses.Api.Data.Dtos;
+using Expenses.Api.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
 
 namespace Expenses.Api.Controllers
@@ -17,10 +20,28 @@ namespace Expenses.Api.Controllers
             _dbContext = dbContext;
         }
         //FIXME: check whether requesting user is authorized
-        [HttpGet]
-        public async Task<ActionResult> GetExpensesAsync()
+        [HttpGet()]
+        public async Task<ActionResult> GetExpensesAsync(int id)
         {
-            
+
+            //abfrage gib mir alle expenses für das event mit der id
+            return null;
+        }
+
+        [HttpPost()]
+        public async Task<ActionResult<ExpenseReadModel>> CreateExpenseAsync(int id, [FromBody] ExpenseWriteModel model)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            var dbEvent = await _dbContext.EventData.FirstOrDefaultAsync(ev => ev.Id == id);
+            if (dbEvent == null) return NotFound();
+
+            var expenseToAdd = _mapper.Map<Expense>(model);
+            _dbContext.ExpenseData.Add(expenseToAdd);
+            dbEvent.Expenses.Add(expenseToAdd);
+
+            // TODO: createdatroute, test and workout
+            return Ok(expenseToAdd);
         }
     }
 }
