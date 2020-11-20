d2018a04954033037cea59907785a95b01118b43
Patrick Mutter <develop@mperia.de>
2020-11-20 23:35:49 +0100

    Add navigation.

--- frontend/src/components/Landing.jsx
+++ frontend/src/components/Landing.jsx
@@ -10,7 +10,7 @@ const Landing = props => {
         <div>
             <h1>Landing</h1>
             <p><Link to='/dashboard'>View Dashboard</Link></p>
-            <p>Logged in status: {allowedAsync() ? "logged in" : "logged out"}</p>
+            {/* <p>Logged in status: {allowedAsync() ? "logged in" : "logged out"}</p> */}
             <button onClick={async () => await signInAsync("spidey", "Abc!23")}>Login</button>
         </div>
     )
---
+++ frontend/src/components/Navigation.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navigation = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <Link className="navbar-brand" to='/'>Expenses</Link>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <Link className="nav-link" to='/dashboard'>Secret 1</Link>
+                        </li>
+                        {/* <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#">Secret 1</a>
+                        </li> */}
+                        <li className="nav-item">
+                            <Link className="nav-link" to='/dashboard'>Secret 2</Link>
+                        </li>
+                    </ul>
+                    <ul className="navbar-nav mr-0 mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <Link className="nav-link" to='/dashboard'>Login</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navigation
