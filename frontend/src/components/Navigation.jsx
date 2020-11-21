86c563c87aacadbecfd677387d2a534740533215
mperator <develop@mperia.de>
2020-11-21 23:50:53 +0100

    Update navigation bar showing items depending on login state.

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import useAuth from '../hooks/useAuth'
 
 const Navigation = () => {
+    const { hasToken } = useAuth();
+
+
+
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
@@ -9,24 +14,40 @@ const Navigation = () => {
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
+
+
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
+
                     <ul className="navbar-nav mr-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <Link className="nav-link" to='/dashboard'>Secret 1</Link>
-                        </li>
-                        {/* <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Secret 1</a>
-                        </li> */}
-                        <li className="nav-item">
-                            <Link className="nav-link" to='/dashboard'>Secret 2</Link>
-                        </li>
+                        {
+                            hasToken ?
+                                <>
+                                    <li className="nav-item">
+                                        <Link className="nav-link" to='/dashboard'>Dashboard</Link>
+                                    </li>
+                                    {/* <li className="nav-item">
+                                        <a className="nav-link active" aria-current="page" href="#">Secret 1</a>
+                                    </li> */}
+                                    <li className="nav-item">
+                                        <Link className="nav-link" to='/dashboard'>Secret 2</Link>
+                                    </li>
+                                </> : null
+                        }
                     </ul>
+
                     <ul className="navbar-nav mr-0 mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <Link className="nav-link" to='/login'>Login</Link>
-                        </li>
+                        {
+                            hasToken ?
+                                <li className="nav-item">
+                                    <button className="nav-link" onClick={() => { console.log("LOGOUT")}}>Logout</button>
+                                </li> :
+                                <li className="nav-item">
+                                    <Link className="nav-link" to='/login'>Login</Link>
+                                </li>
+                        }
                     </ul>
                 </div>
+
             </div>
         </nav>
     )
