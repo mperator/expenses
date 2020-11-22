72f3a46b8b2c89b738a57e0220f1605fb9ec58ab
mperator <develop@mperia.de>
2020-11-22 14:03:02 +0100

    Fix update on unmouted component.

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -3,10 +3,6 @@ import useAuth from '../hooks/useAuth';
 
 import { Redirect } from "react-router-dom";
 
-// TODO: check for error index.js:1 Warning: Can't perform a React state update on an unmounted component. 
-// This is a no-op, but it indicates a memory leak in your application. To fix, cancel all subscriptions 
-// and asynchronous tasks in a useEffect cleanup function. at Login (http://localhost:3000/static/js/main.chunk.js:905:69)
-
 function Login() {
     // check if user is logged in the naviaget to Home (or redirekt)
     const { isLoading, hasToken, loginAsync } = useAuth();
@@ -36,14 +32,6 @@ function Login() {
         // fail -> print error
         try {
             await loginAsync(state.username, state.password)
-
-            setState({
-                ...state,
-                username: "",
-                password: "",
-                error: "",
-            });
-
         } catch (error) {
             setState({
                 ...state,
