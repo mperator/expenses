ac462b32c8fd09a7651c7ecd8a4782de7dfdd7c7
Philipp Halbritter <p.halbritter@logistikbroker.com>
2020-11-23 22:00:56 +0100

    added basic CRUD methods for expenses

--- backend/Expenses.Api/Data/Dtos/ExpenseWriteModel.cs
+++ backend/Expenses.Api/Data/Dtos/ExpenseWriteModel.cs
@@ -1,22 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Expenses.Api.Data.Dtos
 {
     public class ExpenseWriteModel
     {
         #region Properties
+
         [Required]
         public string Title { get; set; }
         public string Description { get; set; }
         [Required]
-        public DateTime Date { get; set; }
+        public string Date { get; set; }
         [Required]
         public float Amount { get; set; }
-        public string Currency { get; set; }
+
         #endregion
     }
 }
