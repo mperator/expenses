1856730be59febd6da28bfc65b847e602e4207c2
Philipp Halbritter <p.halbritter@logistikbroker.com>
2020-11-25 20:55:38 +0100

    Revert "removed migrations"
    This reverts commit 46620dd5d19372123bfeaa1f7a9b2f4ac4ed8647.

---
+++ backend/Expenses.Api/Migrations/20201120204056_Expense.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Expenses.Api.Migrations
+{
+    public partial class Expense : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ExpenseData",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Amount = table.Column<float>(type: "real", nullable: false),
+                    Currency = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IssuerId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    EventId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExpenseData", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ExpenseData_AspNetUsers_IssuerId",
+                        column: x => x.IssuerId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_ExpenseData_EventData_EventId",
+                        column: x => x.EventId,
+                        principalTable: "EventData",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExpenseData_EventId",
+                table: "ExpenseData",
+                column: "EventId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExpenseData_IssuerId",
+                table: "ExpenseData",
+                column: "IssuerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ExpenseData");
+        }
+    }
+}
