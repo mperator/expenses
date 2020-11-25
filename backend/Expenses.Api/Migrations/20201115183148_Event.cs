71b6e435cc3789e30c769eb712b837dd4f51e606
Xalip <thirsty.camel43@gmail.com>
2020-11-25 20:55:38 +0100

    Revert "removed migrations"
    This reverts commit e8d08218a3b0a2b972a6bc82e34184343a0a17d1.

---
+++ backend/Expenses.Api/Migrations/20201115183148_Event.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Expenses.Api.Migrations
+{
+    public partial class Event : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "EventId",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "EventData",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Creator = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Currency = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EventData", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_EventId",
+                table: "AspNetUsers",
+                column: "EventId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_EventData_EventId",
+                table: "AspNetUsers",
+                column: "EventId",
+                principalTable: "EventData",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_EventData_EventId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropTable(
+                name: "EventData");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_EventId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "EventId",
+                table: "AspNetUsers");
+        }
+    }
+}
