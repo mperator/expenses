aad1c4371a245218cc208895d53b039f1645ac5b
mperator <develop@mperia.de>
2020-11-27 17:11:28 +0100

    Add post function.

--- frontend/src/hooks/useClient.js
+++ frontend/src/hooks/useClient.js
@@ -7,7 +7,7 @@ const useClient = () => {
     const history = useHistory();
     const location = useLocation();
 
-    async function getAsync(url, token) {
+    async function getWithAuthenticationAsync(url, token) {
         const response = await fetch(url, {
             method: 'GET',
             headers: {
@@ -33,19 +33,53 @@ const useClient = () => {
                     history.push(`/login?redirectTo=${encodedUri}`)
                     throw "Unauthorized";
                 }
-                return await getAsync(url, renewedToken);
+                return await getWithAuthenticationAsync(url, renewedToken);
             default:
                 console.log("ERROR:", response)
                 return null;
         }
     }
 
+    async function postWithAuthenticationAsync(url, token, data) {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Authorization': `${token}`,
+                'Content-type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
 
+        switch (response.status) {
+            case 200:
+                console.log("valid")
+                return await response.text();
+
+            case 401:
+                console.log("access token invalid, refresh token.")
+                const renewedToken = await renewAccessTokenAsync();
+                console.log("Renewd TRoken", renewedToken)
+                if (!renewedToken) {
+                    const path = location.pathname.substring(1);
+                    const search = location.search;
+                    const uri = path + search;
+                    const encodedUri = encodeURIComponent(uri);
+
+                    history.push(`/login?redirectTo=${encodedUri}`)
+                    throw "Unauthorized";
+                }
+                return await postWithAuthenticationAsync(url, renewedToken, data);
+            default:
+                console.log("ERROR:", response)
+                return null;
+        }
+    }
 
     /* protected routes */
-
+    /* use own hook for auth api */
     const getAuthTestAsync = async () => {
-        return await getAsync('/auth/test', token);
+        return await getWithAuthenticationAsync('/auth/test', token);
     }
 
 
