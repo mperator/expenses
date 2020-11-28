8680e3650d91146ec4ca007605a458471fbc9ad6
Xalip <thirsty.camel43@gmail.com>
2020-11-28 21:55:41 +0100

    deleted unused code lines;

--- backend/Expenses.Api/Profiles/EventProfile.cs
+++ backend/Expenses.Api/Profiles/EventProfile.cs
@@ -12,9 +12,7 @@ namespace Expenses.Api.Profiles
             CreateMap<Event, EventReadModel>()
                 .ForMember(dest => dest.Creator, opt => opt.MapFrom(src => src.Creator.UserName))
                 .ForMember(dest => dest.CreatorId, opt => opt.MapFrom(src => src.Creator.Id));
-            //CreateMap<EventReadModel, Event>().ForMember(dest => dest.);
             CreateMap<Event, EventUpdateModel>().ReverseMap();
-            //CreateMap<User, Creator>().ReverseMap();
         }
     }
 
