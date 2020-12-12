1f54bbd934b9b437c240bdd28c4495ba10fd43eb
Xalip <thirsty.camel43@gmail.com>
2020-12-12 16:27:27 +0100

    rebase develop into ui-event-overview

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react'
-import { useHistory } from 'react-router'
+// import { useHistory } from 'react-router'
+import { Link } from 'react-router-dom'
 import useClient from '../hooks/useClient'
-
+// import EventBodyOverview from './EventBodyOverview'
+import './Dashboard.css'
 
 import Event from './Event'
 
@@ -34,13 +36,27 @@ const Dashboard = () => {
     return (
         <div className="container mt-4">
             <h2>My Events</h2>
-            <button className="btn btn-primary" onClick={create}>New</button>
+            {/* <button className="btn btn-primary" onClick={create}>New</button> */}
 
             <div className="mt-3">
                 {events && events.map(e => (
                     <Event key={e.id} {...e} />
                 ))}
             </div>
+            {/* <div id="addEventDiv"> */}
+            {/* <a href="/create-event"> */}
+            {/* <Redirect to="/create-event"> */}
+
+            {/* <button onClick={create}> */}
+            <Link className="text-dark" to="/create-event">
+                <svg width="1em" height="1em" viewBox="0 0 16 16" className="addButton bi bi-plus-circle-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path fillRule="evenodd" d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z" />
+                </svg>
+            </Link>
+            {/* </Redirect> */}
+            {/* </button> */}
+            {/* </a> */}
+            {/* </div> */}
         </div>
     )
 }
--- frontend/src/components/Event.jsx
+++ frontend/src/components/Event.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import dayjs from 'dayjs'
+import { Link } from 'react-router-dom'
 
-const Event = ({ title, description }) => {
+
+const Event = (props) => {
+
+    const { id, title, description, startDate, endDate } = props;
+
     return (
         <div className="card mb-3">
             <div className="card-body">
                 <h5 className="card-title">{title}</h5>
+                <h6 className="card-subtitle mb-2 text-muted">{dayjs(startDate).format('DD/MM/YYYY')} - {dayjs(endDate).format('DD/MM/YYYY')}</h6>
                 <p className="card-text">{description}</p>
+                <Link to={{ pathname: `/event-view/${id}`, state: { event: props } }} className="stretched-link" />
+                {/* <a href={`/event-view/${id}`} className="stretched-link"></a> */}
             </div>
         </div>
     )
