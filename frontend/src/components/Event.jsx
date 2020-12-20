a109cbf4842a225f1c6d841529d64fc9c8c8fb61
Xalip <thirsty.camel43@gmail.com>
2020-12-20 14:34:10 +0100

    removed some unused code; adjusted route in Link comp;

--- frontend/src/components/Event.jsx
+++ frontend/src/components/Event.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import dayjs from 'dayjs'
 import { Link } from 'react-router-dom'
 
-
 const Event = (props) => {
 
     const { id, title, description, startDate, endDate } = props;
@@ -13,8 +12,7 @@ const Event = (props) => {
                 <h5 className="card-title">{title}</h5>
                 <h6 className="card-subtitle mb-2 text-muted">{dayjs(startDate).format('DD/MM/YYYY')} - {dayjs(endDate).format('DD/MM/YYYY')}</h6>
                 <p className="card-text">{description}</p>
-                <Link to={{ pathname: `/event/editor/${id}`, state: { event: props } }} className="stretched-link" />
-                {/* <a href={`/event-view/${id}`} className="stretched-link"></a> */}
+                <Link to={{ pathname: `/event/view/${id}`, state: { event: props } }} className="stretched-link" />
             </div>
         </div>
     )
