7afbdfc041ec76025994e8221e0980311cfcea91
Xalip <thirsty.camel43@gmail.com>
2020-12-21 21:30:10 +0100

    added -bs- attributes;

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -13,7 +13,7 @@ const Navigation = () => {
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
                 <Link className="navbar-brand" to='/'>Expenses</Link>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
 
