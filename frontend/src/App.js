bda4b76a94090c9aee2d4383e131463ff6bfd504
Xalip <thirsty.camel43@gmail.com>
2020-12-21 19:55:55 +0100

    renamed EventView comp another time; Edit and save event in the same comp;

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,21 @@
 import React from 'react'
 import './App.css';
-
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Landing from './components/Landing'
 import Dashboard from './components/Dashboard'
 import ProtectedRoute from './components/ProtectedRoute';
-
 import { AuthProvider } from './AuthContext';
 import Navigation from './components/Navigation';
 import Login from './components/Login';
 import Info from './components/Info';
 import EventCreate from './components/EventCreate';
 import ExpenseEdit from './components/ExpenseEdit'
-import EventView from './components/EventView';
-import EventEdit from './components/EventEdit';
+import EventViewEdit from './components/EventViewEdit';
 
 function App() {
+
+
+
     return (
         <div className="App">
             <AuthProvider>
@@ -27,9 +27,8 @@ function App() {
                     <ProtectedRoute exact path='/dashboard' component={Dashboard} />
                     <ProtectedRoute exact path='/info' component={Info} />
                     <ProtectedRoute exact path='/event/editor' component={EventCreate} />
-                    <ProtectedRoute path='/event/view/:id' component={EventView} />
+                    <ProtectedRoute path='/event/view/:id' component={EventViewEdit} />
                     <ProtectedRoute exact path='/expense/editor' component={ExpenseEdit} />
-                    <ProtectedRoute path='/event/editor/:id' component={EventEdit} />
                 </Router>
             </AuthProvider>
         </div>
--- frontend/src/components/Event.jsx
+++ frontend/src/components/Event.jsx
@@ -5,14 +5,14 @@ import { Link } from 'react-router-dom'
 const Event = (props) => {
 
     const { id, title, description, startDate, endDate } = props;
-
+    //FIXME: only send event id
     return (
         <div className="card mb-3">
             <div className="card-body">
                 <h5 className="card-title">{title}</h5>
                 <h6 className="card-subtitle mb-2 text-muted">{dayjs(startDate).format('DD/MM/YYYY')} - {dayjs(endDate).format('DD/MM/YYYY')}</h6>
                 <p className="card-text">{description}</p>
-                <Link to={{ pathname: `/event/view/${id}`, state: { event: props } }} className="stretched-link" />
+                <Link to={{ pathname: `/event/view/${id}`, state: { id: id } }} className="stretched-link" />
             </div>
         </div>
     )
