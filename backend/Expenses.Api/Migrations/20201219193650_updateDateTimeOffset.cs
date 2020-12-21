3d1317737b321975074384d9971f5b4787db8975
Xalip <thirsty.camel43@gmail.com>
2020-12-21 19:55:00 +0100

    migration for start and end date; changed type from DateTime to DateTimeOffset;

---
+++ backend/Expenses.Api/Migrations/20201219193650_updateDateTimeOffset.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Expenses.Api.Migrations
+{
+    public partial class updateDateTimeOffset : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "StartDate",
+                table: "EventData",
+                type: "datetimeoffset",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "EndDate",
+                table: "EventData",
+                type: "datetimeoffset",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "StartDate",
+                table: "EventData",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetimeoffset");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EndDate",
+                table: "EventData",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetimeoffset");
+        }
+    }
+}
