ca5871ba80125bd1bb3871ccd9501962ac1c0dc4
mperator <develop@mperia.de>
2020-12-22 19:55:14 +0100

    Remove getByIdWithAuthencticationAsync because not needed.

--- frontend/src/hooks/useClient.js
+++ frontend/src/hooks/useClient.js
@@ -39,38 +39,6 @@ const useClient = () => {
         }
     }
 
-    async function getByIdWithAuthenticationAsync(url, token) {
-        const response = await fetch(url, {
-            method: 'GET',
-            headers: {
-                'Authorization': `${token}`
-            }
-        })
-
-        switch (response.status) {
-            case 200:
-                console.log("valid")
-                return await response.json();
-            case 401:
-                console.log("access token invalid, refresh token.")
-                const renewedToken = await renewAccessTokenAsync();
-                console.log("Renewd TRoken", renewedToken)
-                if (!renewedToken) {
-                    const path = location.pathname.substring(1);
-                    const search = location.search;
-                    const uri = path + search;
-                    const encodedUri = encodeURIComponent(uri);
-
-                    history.push(`/login?redirectTo=${encodedUri}`)
-                    throw "Unauthorized";
-                }
-                return await getByIdWithAuthenticationAsync(url, renewedToken);
-            default:
-                console.log("ERROR:", response)
-                return null;
-        }
-    }
-
     async function postWithAuthenticationAsync(url, token, data) {
         const response = await fetch(url, {
             method: 'POST',
@@ -180,7 +148,7 @@ const useClient = () => {
     }
 
     const getEventByIdAsync = async (id) => {
-        return await getByIdWithAuthenticationAsync(`/events/${id}`, token)
+        return await getWithAuthenticationAsync(`/events/${id}`, token)
     }
 
     return {
