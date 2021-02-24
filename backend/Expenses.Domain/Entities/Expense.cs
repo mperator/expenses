530cd6fbb94890064895d301df9ef5a0a4f8f1ea
Patrick Mutter <develop@mperia.de>
2021-02-24 21:13:26 +0100

    Add domain objects.

---
+++ backend/Expenses.Domain/Entities/Expense.cs
@@ -0,0 +1,22 @@
+﻿using Expenses.Domain.Common;
+using System;
+
+namespace Expenses.Domain.Entities
+{
+    public class Expense : AuditableEntity
+    {
+        #region Properties
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public float Amount { get; set; }
+        public string Currency { get; set; }
+        public string IssuerId { get; set; }
+        public User Issuer { get; set; }
+        public int EventId { get; set; }
+        public Event Event { get; set; }
+
+        #endregion
+    }
+}
---
+++ backend/Expenses.Domain/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿using Expenses.Domain.Common;
+using System;
+using System.Collections.Generic;
+
+namespace Expenses.Domain.Entities
+{
+    public class User : AuditableEntity
+    {
+        #region Properties
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime? DateOfBirth { get; set; }
+
+        public IList<Event> Events { get; set; }
+
+        #endregion
+    }
+}
