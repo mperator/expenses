004ef7115e316fd4cb4c5599cf4ec4f46827129b
mperator <develop@mperia.de>
2021-02-24 22:01:15 +0100

    Current state.

---
+++ backend/Expenses.Application/Common/Models/TokenModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Expenses.Application.Common.Models
+{
+    public class TokenModel
+    {
+        public string TokenType { get; set; }
+        public string AccessToken { get; set; }
+        public DateTime Expires { get; set; }
+
+        public string RefreshToken { get; set; }
+        public DateTime RefreshTokenExpires { get; set; }
+    }
+}
---
+++ backend/Expenses.Infrastructure/Identity/IdentityService.cs
@@ -0,0 +1,35 @@
+﻿using Expenses.Application.Common.Interfaces;
+using Expenses.Application.Common.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace Expenses.Infrastructure.Identity
+{
+    public class IdentityService : IIdentityService
+    {
+        public Task<Result> ConfirmEmail(string email, string token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<(Result Result, TokenModel TokenModel)> HandleRefreshTokenAsync(string refreshToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<(Result Result, TokenModel TokenModel)> LoginAsync(string username, string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> LogoutAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result> RegisterAsync(string firstName, string lastName, string username, string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
