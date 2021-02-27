93f4682bdfea49a2e1cdd7c09c1af3a4223a62bc
mperator <develop@mperia.de>
2021-02-27 22:00:21 +0100

    Migrate attendees to cqrs.

---
+++ backend/Expenses.Application/Attendees/Queries/GetAttendees/AttendeeFilter.cs
@@ -0,0 +1,7 @@
+﻿namespace Expenses.Application.Attendees.Queries.GetAttendees
+{
+    public class AttendeeFilter
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ backend/Expenses.Application/Attendees/Queries/GetAttendees/GetAttendeesAttendeeReadModel.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Expenses.Application.Common.Mappings;
+using Expenses.Domain.Entities;
+
+namespace Expenses.Application.Attendees.Queries.GetAttendees
+{
+    public class GetAttendeesAttendeeReadModel : IMapFrom<User>
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<User, GetAttendeesAttendeeReadModel>()
+                .ForMember(d => d.Name, o => o.MapFrom(s => $"{s.FirstName} {s.LastName}"));
+        }
+    }
+}
