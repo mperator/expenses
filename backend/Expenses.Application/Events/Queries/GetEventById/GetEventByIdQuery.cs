8d0be5bb45d86509568f5b65807ab16fadaa87c1
Xalip <thirsty.camel43@gmail.com>
2021-02-27 21:40:48 +0100

    implemented GetEventById query;

---
+++ backend/Expenses.Application/Events/Queries/GetEventById/GetEventByIdQuery.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Expenses.Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Expenses.Application.Events.Queries.GetEventById
+{
+    public class GetEventByIdQuery : IRequest<EventReadModel> {
+        public int Id { get; set; }
+    }
+
+    public class GetEventByIdQueryHandler : IRequestHandler<GetEventByIdQuery, EventReadModel>
+    {
+        private readonly IAppDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetEventByIdQueryHandler(IAppDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<EventReadModel> Handle(GetEventByIdQuery request, CancellationToken cancellationToken)
+        {
+            return await _context.EventData
+                .Include(ev => ev.Creator)
+                .Include(e => e.Attendees)
+                .Include(ev => ev.Expenses)
+                .AsSingleQuery()
+                .ProjectTo<EventReadModel>(_mapper.ConfigurationProvider)
+                .SingleOrDefaultAsync(ev => ev.Id == request.Id);
+        }
+    }
+}
