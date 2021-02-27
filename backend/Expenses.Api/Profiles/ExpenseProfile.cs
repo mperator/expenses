e0025822611b421586a0c53f580fa128ceccbdcd
Xalip <thirsty.camel43@gmail.com>
2021-02-27 16:17:50 +0100

    commented out some mappings;

--- backend/Expenses.Api/Profiles/ExpenseProfile.cs
+++ backend/Expenses.Api/Profiles/ExpenseProfile.cs
@@ -1,10 +1,4 @@
 ﻿using AutoMapper;
-using Expenses.Api.Data.Dtos;
-using Expenses.Api.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Expenses.Api.Profiles
 {
@@ -12,9 +6,9 @@ namespace Expenses.Api.Profiles
     {
         public ExpenseProfile()
         {
-            CreateMap<Expense, ExpenseReadModel>().ReverseMap();
-            CreateMap<Expense, ExpenseWriteModel>().ReverseMap();
-            CreateMap<Expense, ExpenseUpdateModel>().ReverseMap();
+            //CreateMap<Expense, ExpenseReadModel>().ReverseMap();
+            //CreateMap<Expense, ExpenseWriteModel>().ReverseMap();
+            //CreateMap<Expense, ExpenseUpdateModel>().ReverseMap();
         }
     }
 }
