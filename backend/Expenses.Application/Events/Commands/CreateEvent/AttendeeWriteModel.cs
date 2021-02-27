dc930ee21cc9bbea2a20d7f5e099791d4113db3b
Xalip <thirsty.camel43@gmail.com>
2021-02-27 20:32:20 +0100

    added CreateEvent command

---
+++ backend/Expenses.Application/Events/Commands/CreateEvent/AttendeeWriteModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Expenses.Application.Events.Commands.CreateEvent
+{
+    public class AttendeeWriteModel
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ backend/Expenses.Application/Events/Commands/CreateEvent/CreateEventCommand.cs
@@ -0,0 +1,81 @@
+﻿using Expenses.Application.Common.Interfaces;
+using Expenses.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Expenses.Application.Events.Commands.CreateEvent
+{
+    // FIXME: should the response type really be an int??
+    public class CreateEventCommand : IRequest<int>
+    {
+        #region Properties
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Currency { get; set; }
+
+        public DateTimeOffset StartDate { get; set; }
+
+        public DateTimeOffset EndDate { get; set; }
+
+        public IEnumerable<AttendeeWriteModel> Attendees { get; set; }
+
+        #endregion
+    }
+
+    public class CreateEventCommandHandler : IRequestHandler<CreateEventCommand, int>
+    {
+        private readonly IAppDbContext _context;
+        private readonly ICurrentUserService _currentUserService;
+        private readonly IUserService _userService;
+
+        public CreateEventCommandHandler(IAppDbContext context, ICurrentUserService currentUserService,
+            IUserService userService)
+        {
+            _context = context;
+            _currentUserService = currentUserService;
+            _userService = userService;
+        }
+
+        public async Task<int> Handle(CreateEventCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _userService.GetCurrentUserAsync();
+
+            Event savedEvent = new Event
+            {
+                Title = request.Title,
+                Description = request.Description,
+                Creator = user,
+                Currency = "Euro",
+                StartDate = request.StartDate,
+                EndDate = request.EndDate
+            };
+            savedEvent.Attendees = new List<User>();
+            savedEvent.Attendees.Add(user);
+            //FIXME:
+            foreach (var a in request.Attendees)
+            {
+                var attendee = await _userService.FindByIdAsync(a.Id);
+                savedEvent.Attendees.Add(attendee);
+            }
+
+            _context.EventData.Add(savedEvent);
+            try
+            {
+                await _context.SaveChangesAsync(cancellationToken);
+            }
+            catch (Exception e)
+            {
+                new InvalidOperationException(e.Message);
+            }
+            //FIXME:
+            //return CreatedAtRoute(nameof(GetEventByIdAsync), new { id = savedEvent.Id }, _mapper.Map<EventReadModel>(savedEvent));
+            return savedEvent.Id;
+        }
+    }
+}
