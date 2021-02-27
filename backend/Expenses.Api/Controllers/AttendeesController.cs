978cd2f73c24d0a2c9dbfda70b5cbf58dcaa389a
Patrick Mutter <develop@mperia.de>
2021-02-27 22:28:16 +0100

    Add authorize to attendees controller.

--- backend/Expenses.Api/Controllers/AttendeesController.cs
+++ backend/Expenses.Api/Controllers/AttendeesController.cs
@@ -1,10 +1,12 @@
 ﻿using Expenses.Application.Attendees.Queries.GetAttendees;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Expenses.Api.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("api/[controller]")]
     public class AttendeesController : ApiControllerBase
