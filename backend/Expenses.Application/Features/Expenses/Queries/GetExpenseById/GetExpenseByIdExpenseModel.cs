b7a38adf3031876e0a0fd125ac139d50b6e68e0a
Patrick Mutter <develop@mperia.de>
2021-02-28 00:44:55 +0100

    Migrate get expense to CQRS.

---
+++ backend/Expenses.Application/Features/Expenses/Queries/GetExpenseById/GetExpenseByIdExpenseModel.cs
@@ -0,0 +1,21 @@
+﻿using Expenses.Application.Common.Mappings;
+using Expenses.Domain.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace Expenses.Application.Features.Expenses.Queries.GetExpenseById
+{
+    public class GetExpenseByIdExpenseModel : IMapFrom<Expense>
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public float Amount { get; set; }
+        public string Currency { get; set; }
+        public string IssuerId { get; set; }
+        public string Issuer { get; set; }
+        public int EventId { get; set; }
+        public ICollection<GetExpenseByIdExpenseUserModel> ExpensesUsers { get; set; }
+    }
+}
---
+++ backend/Expenses.Application/Features/Expenses/Queries/GetExpenseById/GetExpenseByIdQuery.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using Expenses.Application.Common.Exceptions;
+using Expenses.Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Expenses.Application.Features.Expenses.Queries.GetExpenseById
+{
+    public class GetExpenseByIdQuery : IRequest<GetExpenseByIdExpenseModel>
+    {
+        public int EventId { get; set; }
+        public int ExpenseId { get; set; }
+    }
+
+    public class GetExpenseByIdQueryHandler : IRequestHandler<GetExpenseByIdQuery, GetExpenseByIdExpenseModel>
+    {
+        private readonly IAppDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetExpenseByIdQueryHandler(IAppDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<GetExpenseByIdExpenseModel> Handle(GetExpenseByIdQuery request, CancellationToken cancellationToken)
+        {
+            var expense = await _context.ExpenseData
+                .FirstOrDefaultAsync(ex => ex.EventId == request.EventId && ex.Id == request.ExpenseId);
+
+            if (expense == null)
+                throw new NotFoundException($"No expense found for id {request.ExpenseId} on event {request.EventId}");
+
+            return _mapper.Map<GetExpenseByIdExpenseModel>(expense);
+        }
+    }
+}
