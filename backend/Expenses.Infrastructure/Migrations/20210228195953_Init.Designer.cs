f6abee0bedb8e237a479922fb11e1d109bd4e04b
Patrick Mutter <develop@mperia.de>
2021-03-01 20:18:32 +0100

    Add migration.

--- backend/Expenses.Infrastructure/Migrations/20210228195953_Init.Designer.cs
+++ backend/Expenses.Infrastructure/Migrations/20210228195953_Init.Designer.cs
@@ -4,14 +4,16 @@ using Expenses.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Expenses.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210228195953_Init")]
+    partial class Init
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
