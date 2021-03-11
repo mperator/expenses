5155d415b1fd7c3b1f389c3718f7ff38bb5cce05
mperator <develop@mperia.de>
2021-03-11 22:49:02 +0100

    Add validaiton exception to middleware.

--- backend/Expenses.Api/Middlewares/HttpExceptionMiddleware.cs
+++ backend/Expenses.Api/Middlewares/HttpExceptionMiddleware.cs
@@ -1,5 +1,6 @@
 ﻿using Expenses.Application.Common.Exceptions;
 using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
 using System;
 using System.Net;
 using System.Threading.Tasks;
@@ -28,19 +29,18 @@ namespace Expenses.Api.Middlewares
             }
         }
 
+        // https://stackoverflow.com/questions/54104138/mediatr-fluent-validation-response-from-pipeline-behavior
         private static Task HandleException(HttpContext context, Exception ex)
         {
             HttpStatusCode code = HttpStatusCode.InternalServerError; // 500 if unexpected
 
-            // Specify different custom exceptions here
-            //if (ex is NotFoundException) code = HttpStatusCode.BadRequest;
-
-            //string result = JsonConvert.SerializeObject(new { error = ex.Message });
-
-            //context.Response.ContentType = "application/json";
-            //context.Response.StatusCode = (int)code;
-
-            //return context.Response.WriteAsync(result);
+            if (ex is FluentValidation.ValidationException vex)
+            {
+                var result = JsonConvert.SerializeObject(vex.Errors);
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                return context.Response.WriteAsync(result);
+            }
 
             if (ex is NotFoundException) code = HttpStatusCode.NotFound;
             context.Response.StatusCode = (int)code;
