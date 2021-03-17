e28f39a3ff26bf0b45f49309f43fa32f4d8c8029
Patrick Mutter <develop@mperia.de>
2021-03-17 21:10:04 +0100

    Create new domain entity structure and add tests.

---
+++ backend/Expenses.Domain/Entities/Expense.cs
@@ -0,0 +1,52 @@
+﻿using Expenses.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Expenses.Domain.Entities
+{
+    public class Expense
+    {
+        private Credit _credit;
+        private IReadOnlyList<Debit> _debits;
+
+        public int Id { get; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Currency { get; }
+        public DateTime Date { get; set; }
+        public UserId CreatorId { get; }
+
+        public Credit Credit => _credit;
+        public IReadOnlyList<Debit> Debits => _debits;
+
+        private Expense() { } // EF
+
+        public Expense(UserId creatorId, string title, string description, DateTime date, string currency)
+        {
+            // domain validation
+            if (creatorId == null) throw new Exception("Invalid creator.");
+            if (string.IsNullOrWhiteSpace(title)) throw new Exception("Invalid title.");
+            if (string.IsNullOrWhiteSpace(description)) throw new Exception("Invalid description.");  // TODO: allow null?
+            if (date == default) throw new Exception("Invalid date.");
+            if (string.IsNullOrWhiteSpace(currency)) throw new Exception("Invalid currency set.");
+            if(currency.Length != 3) throw new Exception("No valid currency string.");
+
+            CreatorId = creatorId;
+            Title = title;
+            Description = description;
+            Date = date;
+            Currency = currency;
+        }
+
+        public void Split(Credit credit, List<Debit> debits)
+        {
+            // != null
+
+            // TODO validate credit - debits = 0;
+
+            _credit = credit;
+            _debits = debits.AsReadOnly();
+        }
+    }
+}
---
+++ backend/Expenses.Domain/Entities/Split.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Expenses.Domain.Entities
+{
+    class Split
+    {
+    }
+}
