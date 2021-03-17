fc6dbcb28acbb371f91986ae6ed62400b4bdbda0
mperator <develop@mperia.de>
2021-03-17 21:10:04 +0100

    Create new domain entity structure and add tests.

---
+++ backend/Expenses.Domain/Entities/Event.cs
@@ -0,0 +1,32 @@
+﻿using Expenses.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Expenses.Domain.Entities
+{
+    public class Event
+    {
+        private Event() { } // EF
+
+        public Event(string title, string description, UserId creatorId)
+        {
+
+        }
+
+        public void AddCreditor(UserId creditorId, decimal amount)  // Money
+        {
+
+        }
+
+        public void AddDebitor(UserId debitorId, decimal amount)  // Money
+        {
+
+        }
+
+
+        // contains list of participants eg user ids
+
+        // list of expenses
+    }
+}
---
+++ backend/Expenses.Domain/ValueObjects/Debit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Expenses.Domain.ValueObjects
+{
+    public class Debit
+    {
+        public UserId DebitorId { get; set; }
+
+        public decimal Amount { get; set; }
+    }
+}
