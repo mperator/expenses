f161b364e3d68a98a3b1d3b481a0a26ff0ec2d7b
mperator <develop@mperia.de>
2021-03-22 20:39:03 +0100

    Add currency validation.

---
+++ backend/Expenses.Domain/ValueObjects/Currency.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Expenses.Domain.ValueObjects
+{
+    public class Currency
+    {
+        public string Code { get; }
+
+        public Currency(string code)
+        {
+            if (string.IsNullOrWhiteSpace(code)) throw new Exception("Invalid code.");
+            if (!(code.Length == 3)) throw new Exception("Length not invalid");
+
+            Code = code;
+        }
+    }
+}
