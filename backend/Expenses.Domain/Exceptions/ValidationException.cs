a392e2ff282b5e29afccea210bdd419816ceb0a3
Patrick Mutter <develop@mperia.de>
2021-03-22 21:41:00 +0100

    Add custom exceptions for event and expense.

---
+++ backend/Expenses.Domain/Exceptions/ValidationException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Expenses.Domain.Exceptions
+{
+    public abstract class ValidationException : Exception
+    {
+        public string Code { get; }
+
+        protected ValidationException(string code, string message) : base(message)
+        {
+            Code = code;
+        }
+
+        protected ValidationException(string code, string message, Exception innerException) : base(message, innerException)
+        {
+            Code = code;
+        }
+    }
+}
