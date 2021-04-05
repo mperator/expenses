457f07bbacfdc0aca78a51ea234fbc76054a4748
Patrick Mutter <develop@mperia.de>
2021-04-05 18:39:33 +0200

    Fix navigation property many to many relation ship.

--- backend/Expenses.Domain/Entities/User.cs
+++ backend/Expenses.Domain/Entities/User.cs
@@ -1,16 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace Expenses.Domain.Entities
 {
-    // https://stackoverflow.com/questions/39771808/entity-framework-core-many-to-many-relationship-with-same-entity
     public class User
     {
-        public string Id { get; }
+        // Navigation property needed by EFCore5: https://docs.microsoft.com/en-us/ef/core/modeling/relationships?tabs=fluent-api%2Cfluent-api-simple-key%2Csimple-key#many-to-many
+        // However EF6 allows having no navigation property: https://stackoverflow.com/questions/39771808/entity-framework-core-many-to-many-relationship-with-same-entity
+        private IList<Event> Events { get; }
 
-        //// needed by EF Core
-        public IList<Event> Events { get; }
+        public string Id { get; }
 
         public User(string id)
         {
