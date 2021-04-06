95deadf98d977bb567f44fd6be81ac831c5b8110
mperator <develop@mperia.de>
2021-04-07 01:00:14 +0200

    Fix expense create command.

--- backend/Expenses.Application/Features/Expenses/Commands/CreateExpense/CreateExpenseCommandValidator.cs
+++ backend/Expenses.Application/Features/Expenses/Commands/CreateExpense/CreateExpenseCommandValidator.cs
@@ -7,7 +7,27 @@ namespace Expenses.Application.Features.Expenses.Commands.CreateExpense
         // https://danielwhittaker.me/2016/04/20/how-to-validate-commands-in-a-cqrs-application/
         public CreateExpenseCommandValidator()
         {
-            RuleFor(e => e.Model).NotNull().SetValidator(new CreateExpenseRequestExpenseValidator());
+            RuleFor(e => e.EventId).GreaterThan(0).WithMessage("No event specified.");
+            RuleFor(e => e.Model)
+                .NotNull()
+                .ChildRules(request =>
+                {
+                    request.RuleFor(e => e.Title).NotNull().WithMessage("Title is required.");
+                    request.RuleFor(e => e.Description).NotNull().WithMessage("Description is required.");
+                    request.RuleFor(e => e.Date).NotNull().WithMessage("Date is required.");
+
+                    request.RuleFor(e => e.Credit).ChildRules(credit =>
+                    {
+                        credit.RuleFor(e => e.CreditorId).NotNull().WithMessage("Debitor is required.");
+                        credit.RuleFor(e => e.Amount).GreaterThanOrEqualTo(0).WithMessage("Amount is required.");
+                    });
+
+                    request.RuleForEach(e => e.Debits).ChildRules(debit =>
+                    {
+                        debit.RuleFor(e => e.DebitorId).NotNull().WithMessage("Debitor is required.");
+                        debit.RuleFor(e => e.Amount).GreaterThanOrEqualTo(0).WithMessage("Amount is required.");
+                    });
+                });
         }
     }
 }
--- backend/Expenses.Application/Features/Expenses/Commands/CreateExpense/CreateExpenseRequestExpenseParticipant.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Expenses.Application.Features.Expenses.Commands.CreateExpense
-{
-    public class CreateExpenseRequestExpenseParticipant
-    {
-        public string Id { get; set; }
-        public string Name { get; set; }
-        public double Amount { get; set; }
-    }
-}
