5983673052713bc266e21b42906e5a8698757708
mperator <develop@mperia.de>
2021-04-07 01:08:04 +0200

    Fix bug in automapper profile.

--- backend/Expenses.Application/Features/Events/Queries/GetEventById/GetEventByIdQueryExpense.cs
+++ backend/Expenses.Application/Features/Events/Queries/GetEventById/GetEventByIdQueryExpense.cs
@@ -1,4 +1,5 @@
-﻿using Expenses.Application.Common.Mappings;
+﻿using AutoMapper;
+using Expenses.Application.Common.Mappings;
 using Expenses.Domain.Entities;
 using System;
 using System.Collections.Generic;
@@ -12,10 +13,15 @@ namespace Expenses.Application.Features.Events.Queries.GetEventById
         public string Title { get; set; }
         public string Description { get; set; }
         public DateTime Date { get; set; }
-        public decimal Amount { get; set; }
         public string Currency { get; set; }
 
         public GetEventByIdQueryExpenseCredit Credit {get; set;}
         public IEnumerable<GetEventByIdQueryExpenseDebit> Debits {get; set;}
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Expense, GetEventByIdQueryExpense>()
+                .ForMember(d => d.CreatorId, o => o.MapFrom(s => s.Creator.Id));
+        }
     }
 }
