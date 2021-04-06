5cf2c1980016d6e9742222eed3ce37bb795ddb95
mperator <develop@mperia.de>
2021-04-06 21:48:20 +0200

    Update event validator.

--- backend/Expenses.Application/Features/Events/Commands/CreateEvent/CreateEventCommandValidator.cs
+++ backend/Expenses.Application/Features/Events/Commands/CreateEvent/CreateEventCommandValidator.cs
@@ -1,25 +1,30 @@
-﻿using Expenses.Application.Common.Interfaces;
-using FluentValidation;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FluentValidation;
 
 namespace Expenses.Application.Features.Events.Commands.CreateEvent
 {
     public class CreateEventCommandValidator : AbstractValidator<CreateEventCommand>
     {
-        private readonly IAppDbContext _context;
-
-        public CreateEventCommandValidator(IAppDbContext context)
+        public CreateEventCommandValidator()
         {
-            _context = context;
-
-            // validate incoming event
             RuleFor(e => e.Title)
-                .NotEmpty().WithMessage("Title is required.");
-            //FIXME: validate all props
+                .NotEmpty()
+                .WithMessage("Title is required.");
+
+            RuleFor(e => e.Description)
+                .NotEmpty()
+                .WithMessage("Description is required.");
+
+            RuleFor(e => e.Currency)
+                .Length(3)
+                .WithMessage("Currency must be providet in ISO 4217.");
+
+            RuleFor(e => e.StartDate)
+                .NotEmpty()
+                .WithMessage("Start date not set.");
+
+            RuleFor(e => e.EndDate)
+                .NotEmpty()
+                .WithMessage("End date not set.");
         }
     }
 }
