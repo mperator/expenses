1259767634abeea0bd431d2f3a4a3513a7fe5c0a
mperator <develop@mperia.de>
2021-04-06 22:36:34 +0200

    Fix event query.

--- backend/Expenses.Application/Features/Events/Queries/GetEvents/GetEventsQuery.cs
+++ backend/Expenses.Application/Features/Events/Queries/GetEvents/GetEventsQuery.cs
@@ -3,18 +3,15 @@ using AutoMapper.QueryableExtensions;
 using Expenses.Application.Common.Interfaces;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
-using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace Expenses.Application.Features.Events.Queries.GetEvents
 {
-    public class GetEventsQuery : IRequest<List<EventReadModel>>
-    {
-    }
+    public class GetEventsQuery : IRequest<List<GetEventsQueryEvent>> { }
 
-    public class GetEventsQueryHandler : IRequestHandler<GetEventsQuery, List<EventReadModel>>
+    public class GetEventsQueryHandler : IRequestHandler<GetEventsQuery, List<GetEventsQueryEvent>>
     {
         private readonly IAppDbContext _context;
         private readonly IMapper _mapper;
@@ -25,18 +22,12 @@ namespace Expenses.Application.Features.Events.Queries.GetEvents
             _mapper = mapper;
         }
 
-        public async Task<List<EventReadModel>> Handle(GetEventsQuery request, CancellationToken cancellationToken)
+        public async Task<List<GetEventsQueryEvent>> Handle(GetEventsQuery request, CancellationToken cancellationToken)
         {
-            throw new Exception();
-
-            //return new List<EventReadModel>(
-            //    await _context.EventData
-            //    .Include(ev => ev.Creator)
-            //    .Include(ev => ev.Expenses)
-            //    .Include(ev => ev.Attendees)
-            //    .AsSingleQuery()
-            //    .ProjectTo<EventReadModel>(_mapper.ConfigurationProvider)
-            //    .ToListAsync(cancellationToken));
+            return await _context.Events
+                .AsNoTracking()
+                .ProjectTo<GetEventsQueryEvent>(_mapper.ConfigurationProvider)
+                .ToListAsync(cancellationToken);
         }
     }
 }
