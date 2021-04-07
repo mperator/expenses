519a6b7d292dbee1663fb3570f0ea4ebe9c81a26
mperator <develop@mperia.de>
2021-04-07 16:46:48 +0200

    Add email confirmation command.

---
+++ backend/Expenses.Application/Features/Auth/Commands/ConfirmEmail/ConfirmEmailCommand.cs
@@ -0,0 +1,29 @@
+﻿using Expenses.Application.Common.Interfaces;
+using Expenses.Application.Common.Models;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Expenses.Application.Features.Auth.Commands.ConfirmEmail
+{
+    public class ConfirmEmailCommand : IRequest<Result>
+    {
+        public string Email { get; set; }
+        public string Token { get; set; }
+    }
+
+    public class ConfirmEmailCommandHandler : IRequestHandler<ConfirmEmailCommand, Result>
+    {
+        private readonly IIdentityService _identityService;
+
+        public ConfirmEmailCommandHandler(IIdentityService identityService)
+        {
+            _identityService = identityService;
+        }
+
+        public async Task<Result> Handle(ConfirmEmailCommand request, CancellationToken cancellationToken)
+        {
+            return await _identityService.ConfirmEmailAsync(request.Email, request.Token);
+        }
+    }
+}
