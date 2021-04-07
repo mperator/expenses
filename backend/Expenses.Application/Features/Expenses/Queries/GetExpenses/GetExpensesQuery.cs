e23bbc6cb3db83658b1c013b6482652c39d05c7d
Patrick Mutter <develop@mperia.de>
2021-04-07 15:19:28 +0200

    Fix expense query.

--- backend/Expenses.Application/Features/Expenses/Queries/GetExpenses/GetExpensesQuery.cs
+++ backend/Expenses.Application/Features/Expenses/Queries/GetExpenses/GetExpensesQuery.cs
@@ -1,7 +1,6 @@
 ﻿using AutoMapper;
-using Expenses.Application.Common.Exceptions;
+using AutoMapper.QueryableExtensions;
 using Expenses.Application.Common.Interfaces;
-using Expenses.Domain.EntitiesOld;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
@@ -11,12 +10,12 @@ using System.Threading.Tasks;
 
 namespace Expenses.Application.Features.Expenses.Queries.GetExpenses
 {
-    public class GetExpensesQuery : IRequest<IEnumerable<GetExpensesResponseExpense>>
+    public class GetExpensesQuery : IRequest<IEnumerable<GetExpensesQueryExpense>>
     {
         public int EventId { get; set; }
     }
 
-    public class GetExpensesQueryHandler : IRequestHandler<GetExpensesQuery, IEnumerable<GetExpensesResponseExpense>>
+    public class GetExpensesQueryHandler : IRequestHandler<GetExpensesQuery, IEnumerable<GetExpensesQueryExpense>>
     {
         private readonly IAppDbContext _context;
         private readonly IUserService _userService;
@@ -29,49 +28,13 @@ namespace Expenses.Application.Features.Expenses.Queries.GetExpenses
             _mapper = mapper;
         }
 
-        public async Task<IEnumerable<GetExpensesResponseExpense>> Handle(GetExpensesQuery request, CancellationToken cancellationToken)
+        public async Task<IEnumerable<GetExpensesQueryExpense>> Handle(GetExpensesQuery request, CancellationToken cancellationToken)
         {
-            throw new System.Exception();
-
-            //var @event = await _context.Events
-            //    .AsNoTracking()
-            //    .Include(ev => ev.Expenses)
-            //        .ThenInclude(ex => ex.ExpenseUsers)
-            //    .SingleOrDefaultAsync(ev => ev.Id == request.EventId);
-
-            //if(@event == null)
-            //    throw new NotFoundException($"Event {request.EventId} not found.");
-
-            //// get all users distinct
-            //if(@event.Expenses == null)
-            //{
-            //    return Enumerable.Empty<GetExpensesResponseExpense>();
-            //}
-            //else
-            //{
-            //    var userIds = @event.Expenses.SelectMany(a => a.ExpenseUsers).Select(a => a.UserId);
-            //    var users = new List<User>();
-
-            //    foreach (var id in userIds.Distinct())
-            //        users.Add(await _userService.FindByIdAsync(id));
-
-
-            //    var result = new List<GetExpensesResponseExpense>();
-            //    foreach(var expense in @event.Expenses)
-            //    {
-            //        var temp = _mapper.Map<GetExpensesResponseExpense>(expense);
-            //        temp.Participants = expense.ExpenseUsers.Select(e => new GetExpensesResponseExpenseParticipant
-            //        {
-            //            Id = e.UserId,
-            //            Name = users.Where(u => u.Id == e.UserId).SingleOrDefault()?.Username,
-            //            Amount = e.Amount
-            //        }).ToList();
-
-            //        result.Add(temp);
-            //    }
-
-            //    return result;
-            //}
+            return await _context.Expenses
+                .AsNoTracking()
+                .Where(e => EF.Property<int>(e, "EventId") == request.EventId)
+                .ProjectTo<GetExpensesQueryExpense>(_mapper.ConfigurationProvider)
+                .ToListAsync(cancellationToken);
         }
     }
 }
--- backend/Expenses.Application/Features/Expenses/Queries/GetExpenses/GetExpensesResponseExpenseParticipant.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Expenses.Application.Features.Expenses.Queries.GetExpenses
-{
-    public class GetExpensesResponseExpenseParticipant
-    {
-        public string Id { get; set; }
-        public string Name { get; set; }
-        public double Amount { get; set; }
-    }
-}
