6e7297b423b8417fdc4d75adb2a0f341d45db6cb
Patrick Mutter <develop@mperia.de>
2021-04-08 22:44:22 +0200

    Filter events for calling user.

--- backend/Expenses.Application/Features/Events/Queries/GetEvents/GetEventsQuery.cs
+++ backend/Expenses.Application/Features/Events/Queries/GetEvents/GetEventsQuery.cs
@@ -4,6 +4,7 @@ using Expenses.Application.Common.Interfaces;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -15,16 +16,21 @@ namespace Expenses.Application.Features.Events.Queries.GetEvents
     {
         private readonly IAppDbContext _context;
         private readonly IMapper _mapper;
+        private readonly ICurrentUserService _currentUserService;
 
-        public GetEventsQueryHandler(IAppDbContext context, IMapper mapper)
+        public GetEventsQueryHandler(IAppDbContext context, IMapper mapper, ICurrentUserService currentUserService)
         {
             _context = context;
             _mapper = mapper;
+            _currentUserService = currentUserService;
         }
 
         public async Task<List<GetEventsQueryEvent>> Handle(GetEventsQuery request, CancellationToken cancellationToken)
         {
+            string userId = _currentUserService.UserId;
+
             return await _context.Events
+                .Where(e => e.Participants.Select(e => e.Id).Contains(userId))
                 .AsNoTracking()
                 .ProjectTo<GetEventsQueryEvent>(_mapper.ConfigurationProvider)
                 .ToListAsync(cancellationToken);
