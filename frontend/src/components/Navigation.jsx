567cc03ab88993fe8751e3591fe84b1fea6e2081
mperator <develop@mperia.de>
2021-04-08 19:48:04 +0200

    Add registration page.

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -48,9 +48,14 @@ const Navigation = () => {
                                 <li className="nav-item">
                                     <Logout />
                                 </li> :
+                                <>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to='/register'>Register</Link>
+                                </li>
                                 <li className="nav-item">
                                     <Link className="nav-link" to='/login'>Login</Link>
                                 </li>
+                                </>
                         }
                     </ul>
                 </div>
