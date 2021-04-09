61e7adcffc68981a0ddef2004ebee3550b2206b2
Xalip <thirsty.camel43@gmail.com>
2021-04-09 21:33:06 +0200

    changed label from Attendees to Participants; fixed wrong attendee name property to use username property;

--- frontend/src/components/EventEditor.jsx
+++ frontend/src/components/EventEditor.jsx
@@ -176,14 +176,14 @@ const EventEditor = () => {
                         />
 
                         <div className="mb-3">
-                            <label htmlFor="search" className="form-label">Attendees</label>
+                            <label htmlFor="search" className="form-label">Participants</label>
                             <input className="form-control" id="search" name="search" type="text" value={search.query} onChange={handleSearch} placeholder="Search for users ..." autoComplete="off"></input>
 
                             <div className="list-group">
                                 {search.attendees.map(a => (
                                     <button key={a.id} type="button" className="list-group-item list-group-item-action"
                                         onClick={e => { e.preventDefault(); selectAttendee(a); }}
-                                    >{a.name}</button>
+                                    >{a.username}</button>
                                 ))}
                             </div>
                         </div>
@@ -203,7 +203,7 @@ const EventEditor = () => {
                             <ul className="list-group">
                                 {attendees.map(a => (
                                     <li key={a.id} onClick={() => handleDeleteAttendee(a.id)} className="list-group-item d-flex justify-content-between align-items-center">
-                                        {a.name}
+                                        {a.username}
                                         <button type="button" className="btn" >
                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash-fill" viewBox="0 0 16 16">
                                                 <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
