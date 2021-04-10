3274f15deb98d1fd1dc0999a513de7c7dcb4c3a7
Xalip <thirsty.camel43@gmail.com>
2021-04-10 12:46:31 +0200

    represent returned user object the same way as from usercontroller;

--- backend/Expenses.Application/Features/Events/Queries/GetEventById/GetEventByIdQueryEvent.cs
+++ backend/Expenses.Application/Features/Events/Queries/GetEventById/GetEventByIdQueryEvent.cs
@@ -72,13 +72,8 @@ namespace Expenses.Application.Features.Events.Queries.GetEventById
     public class GetEventByIdQueryParticipant : IMapFrom<AppUser>
     {
         public string Id { get; set; }
-        public string Name { get; set; }
-
-        public void Mapping(Profile profile)
-        {
-            profile.CreateMap<AppUser, GetEventByIdQueryParticipant>()
-                .ForMember(d => d.Id, o => o.MapFrom(s => s.Id))
-                .ForMember(d => d.Name, o => o.MapFrom(s => $"{s.FirstName} {s.LastName}"));
-        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
     }
 }
