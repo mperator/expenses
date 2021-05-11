bc5f8e4cbe2f567d2c5be27dc2be37b635a17784
mperator <develop@mperia.de>
2021-05-11 17:06:47 +0200

    Restructure files.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,13 @@ import React from 'react'
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Landing from './components/Landing'
-import Dashboard from './components/Dashboard'
+import Dashboard from './components/Dashboard/Dashboard'
 import ProtectedRoute from './components/ProtectedRoute';
 import { AuthProvider } from './AuthContext';
 import Navigation from './components/Navigation';
 import Register from './components/Register'
 import Login from './components/Login';
 import Info from './components/Info';
-import EventEditor from './components/EventEditor';
 import ExpenseEditor from './components/ExpenseEditor'
 import ExpenseDetails from './components/ExpenseDetails';
 import 'placeholder-loading/dist/css/placeholder-loading.css';
@@ -26,16 +25,12 @@ function App() {
                 <Router>
                     <Navigation />
                     <Switch>
-                        <Route exact path='/' render={props => <Landing {...props} />} />
+                        <Route exact path='/landing' render={props => <Landing {...props} />} />
                         <Route exact path='/login' component={Login} />
                         <Route exact path='/register' component={Register} />
 
+                        <ProtectedRoute exact path='/' component={Dashboard} />
                         <ProtectedRoute exact path='/dashboard' component={Dashboard} />
-                        <ProtectedRoute exact path='/info' component={Info} />
-                        
-                        {/* obsolete */}
-                        <ProtectedRoute exact path='/event/editor/:id?' component={EventEditor} />
-                        
                         <ProtectedRoute exact path='/event/new' component={EventCreate} />
                         <ProtectedRoute exact path='/event/:id' component={Event} />
                         <ProtectedRoute exact path='/event/:id/edit' component={EventEdit} />
@@ -43,6 +38,8 @@ function App() {
 
                         <ProtectedRoute exact path='/expense/editor:id?' component={ExpenseEditor} />
                         <ProtectedRoute exact path='/expense/:id' component={ExpenseDetails} />
+
+                        <ProtectedRoute exact path='/info' component={Info} />
                     </Switch>
                 </Router>
             </AuthProvider>
