a4592b743c72d7c9f8e6500deeca9f56c21ebf5a
Patrick Mutter <develop@mperia.de>
2021-05-12 11:47:55 +0200

    Add migration script to cascade delete expenses.

---
+++ backend/Expenses.Infrastructure/Migrations/20210512094020_ExpenseDelete.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Expenses.Infrastructure.Migrations
+{
+    public partial class ExpenseDelete : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Expense_Event_EventId",
+                table: "Expense");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Expense_Event_EventId",
+                table: "Expense",
+                column: "EventId",
+                principalTable: "Event",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Expense_Event_EventId",
+                table: "Expense");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Expense_Event_EventId",
+                table: "Expense",
+                column: "EventId",
+                principalTable: "Event",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
