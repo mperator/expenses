c5df087de960d7e7435565a2de49064703c14595
mperator <develop@mperia.de>
2021-06-04 14:13:06 +0200

    Add translation to registration page.

--- frontend/src/translations/de-DE/translations.js
+++ frontend/src/translations/de-DE/translations.js
@@ -3,5 +3,16 @@ export const TRANSLATIONS_DEDE = {
         username: "Benutzername",
         password: "Passwort",
         signIn: "Login"
+    },
+    register: {
+        title: "Registrierung",
+        username: "Benutzername",
+        email: "E-Mail",
+        firstName: "Vorname",
+        lastName: "Nachname",
+        password: "Passwort",
+        passwordConfirmation: "Passwort wiederholen",
+        register: "Registrieren",
+        errorPasswordNoMatch: "Passwort stimmt nicht überein."
     }
 };
--- frontend/src/translations/en-US/translations.js
+++ frontend/src/translations/en-US/translations.js
@@ -3,5 +3,16 @@ export const TRANSLATIONS_ENUS = {
         username: "Username",
         password: "Password",
         signIn: "Sign In"
+    },
+    register: {
+        title: "Register",
+        username: "Username",
+        email: "Email",
+        firstName: "First Name",
+        lastName: "Last Name",
+        password: "Password",
+        passwordConfirmation: "Password confirmation",
+        register: "Register",
+        errorPasswordNoMatch: "Password does not match."
     }
 };
