a951fec0c935d5e0778e979dedc58faa46ad2571
Xalip <thirsty.camel43@gmail.com>
2021-06-05 20:32:32 +0200

    Features/UI tests (#29)
    * added first basic UI test for login page;
    added cypress;
    added data-test attributes to login, logout components;
    
    * fixed broken comment inside test file;
    
    * introduced data-test attribute to FormInput component;
    added data-test attributes to login component;
    added missing default cypress files;
    
    * Update build_test_workflow_develop.yml
    
    * Update build_test_workflow_develop.yml
    
    * Delete package-lock.json
    
    * Delete yarn.lock
    
    * add package-lock again
    
    * added cypress as dev dependency
    
    * move from bootstrap beta to stable version;
    removed @popperjs/core package;
    added bootstrap bundle to index.html;

---
+++ frontend/cypress/integration/login.spec.js
@@ -0,0 +1,14 @@
+/// <reference types="cypress" />
+
+context('Login page', () => {
+
+    //TODO: check whether user exists or create always a user for the test
+    it('Login as Testimann', () => {
+        cy.visit('http://localhost:3000/login')
+        cy.get('[data-test=username-field]').type('testimann')
+        cy.get('[data-test=password-field]').type('TestPasswort1234&')
+        cy.get('[data-test=signIn-button]').click()
+        cy.url().should('include', '/dashboard')
+        cy.get('[data-test=logout-button]').contains('Logout')
+    })
+})
--- frontend/src/components/layout/FormInput.jsx
+++ frontend/src/components/layout/FormInput.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const FormInput = ({ type, id, label, placeholder, value, handleChange, error }) => {
+const FormInput = ({ type, id, label, placeholder, value, handleChange, error, dataTestValue }) => {
     const isValid = (e) => (e || "") && " is-invalid";
 
     return (
@@ -9,7 +9,7 @@ const FormInput = ({ type, id, label, placeholder, value, handleChange, error })
             {type === "textarea" ?
                 <textarea className={"form-control" + isValid(error)} id={id} name={id} placeholder={placeholder} value={value} onChange={handleChange}
                 ></textarea>
-                : <input type={type} className={"form-control" + isValid(error)} id={id} name={id} placeholder={placeholder} value={value} onChange={handleChange} />
+                : <input type={type} className={"form-control" + isValid(error)} id={id} name={id} placeholder={placeholder} value={value} onChange={handleChange} data-test={dataTestValue} />
             }
             {error && <div className="invalid-feedback">{error}</div>}
         </div>
